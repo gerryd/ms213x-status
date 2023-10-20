ade54d60ad529397c7ab0d797e271540b978bd79
Gerry Demaret <gerry@fosdem.org>
2023-10-20 15:37:14 +0200

    status: add basic looping functionality

--- go.mod
+++ go.mod
@@ -3,9 +3,12 @@ module github.com/gerryd/ms213x-status
 go 1.19
 
 require (
-	github.com/BertoldVdb/ms-tools v0.0.0-20221129120332-dd3c7641485a
+	github.com/BertoldVdb/ms-tools v0.0.0-20231004114646-22ddf782f595
 	github.com/alecthomas/kong v0.8.0
 	github.com/sstallion/go-hid v0.14.1
 )
 
-require golang.org/x/sys v0.8.0 // indirect
+require (
+	github.com/google/renameio v1.0.1 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+)
--- status.go
+++ status.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 	"encoding/binary"
 	"encoding/json"
 )
@@ -12,7 +13,9 @@ type Region struct {
 }
 
 type StatusCmd struct {
-	Json   bool   `optional name:"json" help:"Output JSON instead of the default list."`
+	Json      bool   `optional name:"json" help:"Output JSON instead of the default list."`
+	Loop      int    `optional name:"loop" help:"Run in a loop and sleep every N microseconds"`
+	Filename  string `optional name:"filename" help:"Output to a file instead of stdout"`
 }
 
 type outputData struct {
@@ -22,29 +25,44 @@ type outputData struct {
 }
 
 func (s *StatusCmd) Run(c *Context) error {
-	output := &outputData{}
+	next := true
 
-	buf := readmem(c, Region { Region: "RAM", Addr: 0x0000e184 }, 2)
-	output.Width = int(binary.LittleEndian.Uint16(buf))
+	for next {
+		output := &outputData{}
 
-	buf = readmem(c, Region { Region: "RAM", Addr: 0x0000e18c }, 2)
-	output.Height = int(binary.LittleEndian.Uint16(buf))
+		buf := readmem(c, Region { Region: "RAM", Addr: 0x0000e184 }, 2)
+		output.Width = int(binary.LittleEndian.Uint16(buf))
 
-	buf = readmem(c, Region { Region: "RAM", Addr: 0x0000e180 }, 1)
+		buf = readmem(c, Region { Region: "RAM", Addr: 0x0000e18c }, 2)
+		output.Height = int(binary.LittleEndian.Uint16(buf))
 
-	if (len(buf) > 0 && buf[0] > 0) {
-		output.Signal = "yes"
-	} else {
-		output.Signal = "no"
-	}
+		buf = readmem(c, Region { Region: "RAM", Addr: 0x0000e180 }, 1)
 
-	if (s.Json) {
-		data, _ := json.Marshal(output)
-		fmt.Println(string(data))
-	} else {
-		fmt.Printf("width: %d\n", output.Width)
-		fmt.Printf("height: %d\n", output.Height)
-		fmt.Printf("signal: %s\n", output.Signal)
+		if (len(buf) == 0) {
+
+		} else if (buf[0] > 0) {
+			output.Signal = "yes"
+		} else {
+			output.Signal = "no"
+		}
+
+		var p = ""
+
+		if (s.Json) {
+			data, _ := json.Marshal(output)
+			p = string(data)
+
+		} else {
+			p = fmt.Sprintf("width: %d\nheight: %d\nsignal: %s\n", output.Width, output.Height, output.Signal)
+		}
+
+		fmt.Print(p)
+
+		if (s.Loop == 0) {
+			next = false
+		} else {
+			time.Sleep(time.Duration(s.Loop) * time.Millisecond)
+		}
 	}
 
 	return nil
